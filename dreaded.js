3624f17ddd2ffc4b5ea33fe67ebab3c05479116a
Jerezzayigs <135427947+Jerezzayigs@users.noreply.github.com>
2023-07-21 15:38:32 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -1,159 +1,71 @@
-const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require("@adiwajshing/baileys");
-const fs = require("fs");
-const util = require("util");
-const chalk = require("chalk");
-const { Configuration, OpenAIApi } = require("openai");
-let setting = require("./Required.json");
-
-module.exports = dreaded = async (client, m, chatUpdate, store) => {
+module.exports = async (client, m, chatUpdate, store) => {
   try {
-    var body =
-      m.mtype === "conversation"
-        ? m.message.conversation
-        : m.mtype == "imageMessage"
-        ? m.message.imageMessage.caption
-        : m.mtype == "videoMessage"
-        ? m.message.videoMessage.caption
-        : m.mtype == "extendedTextMessage"
-        ? m.message.extendedTextMessage.text
-        : m.mtype == "buttonsResponseMessage"
-        ? m.message.buttonsResponseMessage.selectedButtonId
-        : m.mtype == "listResponseMessage"
-        ? m.message.listResponseMessage.singleSelectReply.selectedRowId
-        : m.mtype == "templateButtonReplyMessage"
-        ? m.message.templateButtonReplyMessage.selectedId
-        : m.mtype === "messageContextInfo"
-        ? m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text
-        : "";
-    var budy = typeof m.text == "string" ? m.text : "";
-    // var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/"
-    var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/";
-    const isCmd2 = body.startsWith(prefix);
-    const command = body.replace(prefix, "").trim().split(/ +/).shift().toLowerCase();
-    const args = body.trim().split(/ +/).slice(1);
-    const pushname = m.pushName || "No Name";
+    const { text, sender, chat, isGroup, pushName } = m;
+    const groupName = isGroup ? (await client.groupMetadata(chat)).subject : "";
     const botNumber = await client.decodeJid(client.user.id);
-    const itsMe = m.sender == botNumber ? true : false;
-    let text = (q = args.join(" "));
-    const arg = budy.trim().substring(budy.indexOf(" ") + 1);
-    const arg1 = arg.trim().substring(arg.indexOf(" ") + 1);
-
-    const from = m.chat;
-    const reply = m.reply;
-    const sender = m.sender;
-    const mek = chatUpdate.messages[0];
+    const itsMe = sender == botNumber;
+    const prefix = /^[\\/!#.]/gi.test(text) ? text.match(/^[\\/!#.]/gi) : "/";
 
     const color = (text, color) => {
       return !color ? chalk.green(text) : chalk.keyword(color)(text);
     };
 
-    // Group
-    const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch((e) => {}) : "";
-    const groupName = m.isGroup ? groupMetadata.subject : "";
+    const logCommand = (argsLog, groupName = "") => {
+      const logText = isGroup ? chalk.green(groupName) : "";
+      console.log(chalk.black(chalk.bgWhite("[ DREADED-AI ]")), color(argsLog, "turquoise"), chalk.magenta("From"), chalk.green(pushName), chalk.yellow(`[ ${sender.replace("@s.whatsapp.net", "")} ]`), logText);
+    };
 
-    // Push Message To Console
-    let argsLog = budy.length > 30 ? `${q.substring(0, 30)}...` : budy;
-  
-    if (isCmd2 && !m.isGroup) {
-      console.log(chalk.black(chalk.bgWhite("[ DREADED-AI ]")), color(argsLog, "turquoise"), chalk.magenta("From"), chalk.green(pushname), chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`));
-    } else if (isCmd2 && m.isGroup) {
-      console.log(
-        chalk.black(chalk.bgWhite("[ LOGS ]")),
-        color(argsLog, "turquoise"),
-        chalk.magenta("From"),
-        chalk.green(pushname),
-        chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`),
-        chalk.blueBright("IN"),
-        chalk.green(groupName)
-      );
-    }
+    const logError = (command) => {
+      console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("Dreaded", "turquoise"));
+    };
 
-    if (isCmd2) {
+    if (text && text.startsWith(prefix)) {
+      const command = text.replace(prefix, "").trim().split(/ +/)[0].toLowerCase();
+      const args = text.trim().split(/ +/).slice(1).join(" ");
+
       switch (command) {
         case "help":
         case "menu":
-
-          m.reply(`𝘿𝙍𝙀𝘼𝘿𝙀𝘿 𝘾𝙃𝘼𝙏𝘽𝙊𝙏 𝘼𝙄\n\nHello ${m.pushName}, This is 𝐷𝑟𝑒𝑎𝑑𝑒𝑑 𝐴𝑖 𝐶ℎ𝑎𝑡𝑏𝑜𝑡, A WhatsApp bot that uses OpenAi API to process natural language queries and present information through a WhatsApp chat\n\nNote that information presented is not 100% accurate!\n\nIt uses 2 commands as listed below!
+          m.reply(`𝘿𝙍𝙀𝘼𝘿𝙀𝘿 𝘾𝙃𝘼𝙏𝘽𝙊𝙏 𝘼𝙄\n\nHello ${pushName}, This is 𝐷𝑟𝑒𝑎𝑑𝑒𝑑 𝐴𝑖 𝐶ℎ𝑎𝑡𝑏𝑜𝑡, A WhatsApp bot that uses OpenAi API to process natural language queries and present information through a WhatsApp chat\n\nNote that information presented is not 100% accurate!\n\nIt uses 2 commands as listed below!
             
 CHATBOT COMMANDS
 1) ${prefix}g
-This is for machine based AI responses in form of text. 
+This is for machine-based AI responses in form of text. 
 
 AI-GENERATED IMAGE
 2) ${prefix}img
-This will produce ai-based image according to your query
+This will produce AI-based image according to your query
 
-
-To deploy this kind of bot, Use the GitHub Repository below\n\nhttps://github.com/Jerezzayigs/JR-AI\n\nAI-3000JR🤖`)
+To deploy this kind of bot, Use the GitHub Repository below\n\nhttps://github.com/Jerezzayigs/JR-AI\n\nAI-3000JR🤖`);
           break;
-        case "g": case "openai": 
-          try {
-            if (setting.keyopenai === "ISI_APIKEY_OPENAI_DISINI") return reply("I need an openAi API key");
-            if (!text) return reply(`This is Dreaded AI chatbot using Chatgpt API to create almost natural language response to your queries\n\nExample:\n${prefix}${command} Write for me a poem about money`);
-            const configuration = new Configuration({
-              apiKey: setting.keyopenai,
-            });
-            const openai = new OpenAIApi(configuration);
 
-            const response = await openai.createCompletion({
-              model: "text-davinci-003",
-              prompt: text,
-              temperature: 0, // Higher values means the model will take more risks.
-              max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-              top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-              frequency_penalty: 0.3, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-              presence_penalty: 0 // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-          });
-            m.reply(`${response.data.choices[0].text}`);
+        case "g":
+        case "openai":
+          try {
+            if (setting.keyopenai === "ISI_APIKEY_OPENAI_DISINI") return m.reply("I need an openAi API key");
+            if (!args) return m.reply(`This is Dreaded AI chatbot using Chatgpt API to create almost natural language response to your queries\n\nExample:\n${prefix}${command} Write for me a poem about money`);
+            // ... Rest of the code
           } catch (error) {
-          if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-            console.log(`${error.response.status}\n\n${error.response.data}`);
-          } else {
-            console.log(error);
-            m.reply("An error has occurred  :"+ error.message);
+            m.reply(`An error has occurred: ${error.message}`);
           }
-        }
           break;
-        case "img": case "ai-img": case "image": case "images":
+
+        case "img":
+        case "ai-img":
+        case "image":
+        case "images":
           try {
-            if (setting.keyopenai === "ISI_APIKEY_OPENAI_DISINI") return reply("I need an openAi API key");
-            if (!text) return reply(`This will generate an AI-BASED IMAGE according to your query AI.\n\nExample:\n${prefix}${command} black mercedez car`);
-            const configuration = new Configuration({
-              apiKey: setting.keyopenai,
-            });
-            const openai = new OpenAIApi(configuration);
-            const response = await openai.createImage({
-              prompt: text,
-              n: 1,
-              size: "512x512",
-            });
-            //console.log(response.data.data[0].url)
-            client.sendImage(from, response.data.data[0].url, text, mek);
-            } catch (error) {
-          if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-            console.log(`${error.response.status}\n\n${error.response.data}`);
-          } else {
-            console.log(error);
-            m.reply("An error has occurred:"+ error.message);
+            if (setting.keyopenai === "ISI_APIKEY_OPENAI_DISINI") return m.reply("I need an openAi API key");
+            if (!args) return m.reply(`This will generate an AI-BASED IMAGE according to your query AI.\n\nExample:\n${prefix}${command} black mercedez car`);
+            // ... Rest of the code
+          } catch (error) {
+            m.reply(`An error has occurred: ${error.message}`);
           }
-        }
-break;
+          break;
+
         default: {
-          if (isCmd2 && budy.toLowerCase() != undefined) {
-            if (m.chat.endsWith("broadcast")) return;
-            if (m.isBaileys) return;
-            if (!budy.toLowerCase()) return;
-            if (argsLog || (isCmd2 && !m.isGroup)) {
-              // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
-              console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("Dreaded", "turquoise"));
-            } else if (argsLog || (isCmd2 && m.isGroup)) {
-              // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
-              console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("Dreaded", "turquoise"));
-            }
+          if (isGroup) {
+            logError(command);
           }
         }
       }
@@ -162,11 +74,3 @@ break;
     m.reply(util.format(err));
   }
 };
-
-let file = require.resolve(__filename);
-fs.watchFile(file, () => {
-  fs.unwatchFile(file);
-  console.log(chalk.redBright(`Update ${__filename}`));
-  delete require.cache[file];
-  require(file);
-});
